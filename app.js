6ac77f126d19feff6f0f02984c7b4bf54dd9ea8e
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2020-10-10 00:48:36 +0530

    Added CORS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const compression = require('compression');
-
+const cors = require('cors');
 const morgan = require('./logging/morgan');
 const routes = require('./routes');
 
@@ -9,6 +9,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(compression());
+app.use(cors());
 
 // Run Migrations
 require('./models/user');
